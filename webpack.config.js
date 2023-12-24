725a960c763e53dae8bf7ec34003bb28429c5647
Axel <axel.montlahuc@stjoavignon.com>
2023-12-24 12:07:24 +0100

    Deployed website

---
+++ webpack.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    entry: "./src/main.js",
+    mode: "development",
+    output: {
+        path: "C:/Users/kerne/Documents/Development/Symplex/MainWebsite/public",
+        filename: "bundle.js",
+    },
+};
